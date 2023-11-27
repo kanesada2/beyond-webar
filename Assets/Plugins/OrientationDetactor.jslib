2f40c0b5b91cdfd2da7611a70a212ecec3e0864a
kanesada2 <kanesada2@users.noreply.github.com>
2023-11-27 18:24:13 +0900

    include game files

---
+++ Assets/Plugins/OrientationDetactor.jslib
@@ -0,0 +1,10 @@
+﻿mergeInto(LibraryManager.library, {
+
+WatchDeviceorientation: function() {
+        window.addEventListener("deviceorientation", function(event) { // ①
+            xy = event.alpha + "," + event.beta + "," + event.gamma; // ②
+            SendMessage('Camera', 'SetRotation', xy); // ③
+        });
+    },  
+
+});
---
+++ Assets/RotationManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using HutongGames.PlayMaker;
+
+public class RotationManager : MonoBehaviour
+{
+    string[] rotations;
+    // JavaScript関数の宣言
+    [DllImport("__Internal")]
+    private static extern void WatchDeviceorientation();
+
+    void Start()
+    {
+        // JavaScriptの呼び出し
+        WatchDeviceorientation();
+    }
+
+    void FixedUpdate()
+    {
+        if(rotations != null)
+        {
+            this.transform.rotation = Quaternion.Euler(/*-1 * float.Parse(rotations[1]) + 90*/30.0f, -float.Parse(rotations[0]), 0.0f);
+        }
+    }
+
+    public void SetRotation(String rotation)
+    {
+        rotations = rotation.Split(',');
+    }
+}
